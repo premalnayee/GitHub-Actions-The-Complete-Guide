fa2fba86e13c8dbfbd176a2310f6569ec8e0d007
premalnayee <premnayee1@gmail.com>
2025-01-10 16:48:59 +0000

    feat: Add AWS S3 deployment action and update workflow configuration

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,9 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as exec from '@actions/exec';
+
+function run() {
+  core.notice('Deploying to S3');
+}
+
+run();
