ce610e16cd3dbe76badcfe599717136196baf38d
premalnayee <premnayee1@gmail.com>
2025-01-10 17:07:22 +0000

    feat: Display AWS access keys during S3 deployment process

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,6 +12,12 @@ function run() {
   const s3uri = `s3://${bucket}/${source}`;
   exec.exec('echo', ['Deploying to', s3uri, 'in', region]);
 
+  // Show the  AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+  exec.exec('echo', ['AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID]);
+  exec.exec('echo', [
+    'AWS_SECRET_ACCESS_KEY:',
+    process.env.AWS_SECRET_ACCESS_KEY,
+  ]);
   core.notice('Deploying to S3');
 }
 
