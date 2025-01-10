ef1ca480529b19861539df4892175f48dc408516
premalnayee <premnayee1@gmail.com>
2025-01-10 17:00:40 +0000

    feat: Add input parameters for S3 deployment action and update main execution logic

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
+  // get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const source = core.getInput('source', { required: true });
+  const region = core.getInput('region', { required: true });
+
+  // Pretend to deploy to S3
+  const s3uri = `s3://${bucket}/${source}`;
+  exec.exec('echo', ['Deploying to', s3uri, 'in', region]);
+
   core.notice('Deploying to S3');
 }
 
